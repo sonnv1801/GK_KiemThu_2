0e07ce450ffb43fbd3690ce1b8ed2b4fcf035709
sonnv1801 <70565643+sonnv1801@users.noreply.github.com>
2022-10-18 10:23:34 +0700

    #2 - update

--- GK_KiemThu_02/app/routers/user.router.js
+++ GK_KiemThu_02/app/routers/user.router.js
@@ -7,10 +7,12 @@ const {
   UpdateOnlyUser,
   DeleteUser,
   LoginUser,
+  GetByIdUser,
 } = require("../controllers/user.controller");
 router.post("/", PostUser);
 router.get("/", GetAllUser);
 router.put("/:id", UpdateOnlyUser);
 router.delete("/:id", DeleteUser);
 router.post("/login", LoginUser);
+router.get("/:id", GetByIdUser);
 module.exports = router;
